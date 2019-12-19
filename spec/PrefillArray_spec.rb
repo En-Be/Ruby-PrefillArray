d24456b3b568d59a3e38806f8dd5ed7ade05fd9e
Nicholas <n3ttl3t@gmail.com>
2019-12-19 12:48:50 +0000

    fifth test passing

--- spec/PrefillArray_spec.rb
+++ spec/PrefillArray_spec.rb
@@ -15,4 +15,8 @@ describe 'PrefillArray' do
     it 'accepts a string of integers as the number of elements' do
         expect(PrefillArray("3",2)).to eq [2,2,2]
     end
+
+    it 'outputs a 2d array when used as a parameter in itself' do
+        expect(PrefillArray(2,PrefillArray(2,1))).to eq [[1,1], [1,1]]
+    end
 end
