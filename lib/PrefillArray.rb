aaa51a3558b02f648744f6de22179f1a3b59dab2
Nicholas <n3ttl3t@gmail.com>
2019-12-19 13:06:41 +0000

    sixth passing test

--- lib/PrefillArray.rb
+++ lib/PrefillArray.rb
@@ -1,6 +1,13 @@
 
 def PrefillArray(n,v)
     result = []
+
+    # print("n = #{n} = #{n.to_i}")
+
+    if n.to_i.to_s != n.to_s
+        raise TypeError.new("#{n} is invalid")  
+    end
+
     for i in 1..n.to_i do
         result.push(v)
     end
